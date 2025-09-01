6e01f1904a3827f5e408b1504c4bab5fb69d1151
manishmh <manishmh982@gmail.com>
2025-09-01 21:21:22 +0530

    first commit by manish

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("I learned git/github");
             System.out.println("second");
+            System.out.println("I like manish kumar");
 
         }
     }
